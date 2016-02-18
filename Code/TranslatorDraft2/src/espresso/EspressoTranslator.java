533959bca8cd04ee21c1530de4d1af5165fee7e0
nathanchen13 <nathanchen13@gmail.com>
2016-02-17 21:28:16 -0500

    Create EspressoTranslator.java
    This class is in charge of taking tranlating TestCase objects into Espresso commands. Currently it only has click capability.

---
+++ Code/TranslatorDraft2/src/espresso/EspressoTranslator.java
@@ -0,0 +1,61 @@
+package Espresso;
+
+import java.util.List;
+import classes.Component;
+import classes.StepTestCase;
+import classes.TestCase;
+
+/**
+ * This is the EspressoTranslator class, it takes Action objects and creates Espresso
+ * commands that correspond to those Action objects.
+ * 
+ * @author Nathan Chen
+ *
+ */
+public class EspressoTranslator {
+	
+	/**
+	 * Constructor for the EspressoTranslator class.
+	 */
+	public EspressoTranslator(){
+	}
+	
+	/**
+	 * Iterates through all the StepTestCase objects in the stepTestCases List.
+	 * @param testCase The current TestCase object being looked at
+	 */
+	public void steps_iterator(TestCase testCase){
+		String appName = testCase.getAppName();
+		String packageName = testCase.getPackageName();
+		String mainActivity = testCase.getMainActivity();
+		List<StepTestCase> stepTestCases = testCase.getSteps();
+		
+		for(int i = 0; i < stepTestCases.size(); i++){
+			StepTestCase cur = stepTestCases.get(i);
+			espresso_switcher(cur.getAction(), cur);
+		}
+	}
+	
+	/**
+	 * Switching hub for taking different kinds of input, calls methods depending
+	 * on what kind of user input is given.
+	 * @param action String of the action to be performed
+	 * @param s StepTestCase object being looked at 
+	 */
+	public void espresso_switcher(String action, StepTestCase s){
+		
+		if(action.equals("CLICK")){
+			make_click_command(s.getComponent());
+		}
+	}
+
+	/**
+	 * Generates the click command formatted in the Espresso framework
+	 * @param c Component object of the current stepTestCase being analyzed
+	 */
+	private void make_click_command(Component c) {
+		String espresso_command = new String();		
+		espresso_command += "onView(withId(R.id." + c.getId().substring(3) + ")).perform(click());";
+		System.out.println(espresso_command);
+	}
+}
