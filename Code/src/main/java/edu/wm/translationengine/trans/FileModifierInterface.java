4b999534ca400626468b4a70271ccccfebe1a04b
nathanchen13 <nathanchen13@gmail.com>
2016-03-21 11:44:16 -0400

    Fix FileModifierInterface
    I updated the interface to include less methods.

--- Code/src/main/java/edu/wm/translationengine/trans/FileModifierInterface.java
+++ Code/src/main/java/edu/wm/translationengine/trans/FileModifierInterface.java
@@ -1,20 +1,12 @@
 package edu.wm.translationengine.trans;
 
 import java.io.IOException;
-import java.util.ArrayList;
 
 public interface FileModifierInterface {
 	
 	public void setupFileImports() throws IOException;
 	
-	public void setupTestMethodHeader();
+	public void setupTestMethodHeader(String packageName, String mainActivity);
 	
-	public void closeTestMethod();
-	
-	public void writeToFile(ArrayList<String> al) throws IOException;
-	
-	public void writeToFile() throws IOException;
-	
-	
-	
+	public void closeTestMethod();	
 }
