cb87c3d6bda2d9235d733a0402ba7bfd4d7d50cf
ttm8 <thotran.public@gmail.com>
2016-04-16 13:20:07 -0400

    Implemented check for Robotium
    Added the checker for Robotium.

---
+++ Code/src/main/java/edu/wm/translationengine/robotium/RobotiumChecker.java
@@ -0,0 +1,119 @@
+package edu.wm.translationengine.robotium;
+
+import edu.wm.translationengine.classes.Component;
+import edu.wm.translationengine.classes.TestCase;
+import edu.wm.translationengine.trans.AbstractChecker;
+
+/**
+ *	Checks if test file contains all necessary info for Robotium
+ *	to run translate.
+ *
+ */
+public class RobotiumChecker extends AbstractChecker {
+
+	public RobotiumChecker() {
+		super();
+	}
+	
+	public boolean checkAppData(TestCase tc) {
+		
+		if(tc.getAppName() == null || tc.getAppName().equals("")) {
+			System.err.println("*** Info needed for test case missing: app name.");
+			System.err.println("Input test case file should include app name as a value for the key \'appName\'");
+			return false;
+		}
+		if(tc.getMainActivity() == null || tc.getMainActivity().equals("")) {
+			System.err.println("*** Info needed for test case missing: main activity");
+			System.err.println("Input test case file should include main activity as a value for the key \'mainActivity\'");
+			return false;
+		}
+		if(tc.getPackageName() == null || tc.getPackageName().equals("")) {
+			System.err.println("*** Info needed for test case missing: package name");
+			System.err.println("Input test case file should include main activity as a value for the key \'packageName\'");
+			return false;
+		}
+	
+		return true;	
+	}
+	
+	public boolean checkClick(Component c) {
+				
+		if(!hasBasicComponentInfo(c, "click"))
+			return false;
+		
+		return true;
+	}
+	
+	public boolean checkLongClick(Component c) {
+		
+		if(!hasBasicComponentInfo(c, "long click"))
+			return false;
+
+		return true;
+	}
+	
+	public boolean checkType(Component c) {
+		
+		if(!hasBasicComponentInfo(c, "long click"))
+			return false;
+		
+		if(c.getText() == null) {
+			// don't check c.getText().equals("") because 
+			// empty strings can be inputted for type action
+			
+			System.err.println("*** Information needed for long click action is missing: id\n" +
+					"Input test case file should include the component id as a value for the key \'text\' in stepTestCases");
+			return false;
+		}
+		
+		return true;
+	}
+	
+	private boolean hasBasicComponentInfo(Component c, String action) {
+		
+		String typeErr = checkType(c, action);
+		if(typeErr != null) {
+			System.err.println(typeErr);
+			return false;
+		}
+		
+		String indexErr = checkIndex(c, action);
+		if(indexErr != null) {
+			System.err.println(indexErr);
+			return false;
+		}
+		
+		return true;
+	}
+	
+	private String checkID(Component c, String action) {
+		
+		if(c.getId() == null || c.equals("")) {
+			return "*** Information needed for " + action + " action is missing: id\n" +
+					"Input test case file should include the component id as a value for the key \'id\' in stepTestCases";
+		}
+		
+		return null;
+	}
+	
+	private String checkType(Component c, String action) {
+		
+		if(c.getType() == null || c.getType().equals("")) {
+			return "*** Information needed for " + action + " action is missing: type\n" +
+					"Input test case file should include the component id as a value for the key \'type\' in stepTestCases";
+		}
+		
+		return null;
+	}
+	
+	private String checkIndex(Component c, String action) {
+		
+		if(c.getIndex() == null || c.getIndex().equals("")) {
+			return "Information needed for " + action + " action is missing: index\n" +
+					"Input test case file should include the component id as a value for the key \'index\' in stepTestCases";
+		}
+		
+		return null;
+	}
+	
+}
--- Code/src/main/java/edu/wm/translationengine/robotium/RobotiumFunctions.java
+++ Code/src/main/java/edu/wm/translationengine/robotium/RobotiumFunctions.java
@@ -1,8 +1,5 @@
 package edu.wm.translationengine.robotium;
 
-import java.util.Arrays;
-import java.util.List;
-
 import edu.wm.translationengine.classes.StepTestCase;
 import edu.wm.translationengine.trans.GenericFunctions;
 
@@ -15,23 +12,7 @@ public class RobotiumFunctions extends GenericFunctions {
 	
 	public void writeStep(StepTestCase testCase) throws Exception {
 		
-		if(testCase == null) {
-			System.out.println("Test case is null");
-			RobotiumTranslator.toWrite.add("\t\t //Error: Test case is null");
-			return;
-		}
-		
 		String action = testCase.getAction();
-		if(action == null) {
-			System.out.println("Action is null");
-			RobotiumTranslator.toWrite.add("\t\t //Error: Test case action is null");
-			return;
-		}
-		
-		if(testCase.getComponent() == null) {
-			System.out.println("Component is null");
-			RobotiumTranslator.toWrite.add("\t\t //Error: Component is null");
-		}
 		
 		RobotiumTranslator.toWrite.add("\n");
 		if(action.equalsIgnoreCase("CLICK")) {
@@ -46,85 +27,66 @@ public class RobotiumFunctions extends GenericFunctions {
 		
 		RobotiumTranslator.toWrite.add("\n");
 	}
-
+	
+	/**
+	 * Need: type, index
+	 */
 	@Override
 	public boolean tap(StepTestCase testCase) throws Exception {
+
+		String id = testCase.getComponent().getId();
+		String type = testCase.getComponent().getType();
+		String index = testCase.getComponent().getIndex();
+
+		writeTestName(id);
 		
-		try 
+		if(type.equals("com.android.systemui.statusbar.policy.KeyButtonView")) 
 		{
-			String id = testCase.getComponent().getId();
-			String type = testCase.getComponent().getType();
-			String index = testCase.getComponent().getIndex();
-			
-			throwEmptyInfo(Arrays.asList(id,type,index));
-			
-			RobotiumTranslator.toWrite.add("\t\t// " + id + "\n");
-			
-			if(type.equalsIgnoreCase("com.android.systemui.statusbar.policy.KeyButtonView")) 
-			{
-				RobotiumTranslator.toWrite.add("\t\tsolo.goBack();\n");
-			}
-			else 
-			{
-				RobotiumTranslator.toWrite.add("\t\tcurView = solo.getView(" + type + ".class, " + index + ");\n");
-				RobotiumTranslator.toWrite.add("\t\tsolo.clickOnView(curView);");
-			}
-			
-		} catch(Exception e) {
-			System.out.println("Information needed for tap action is missing. Error: " + e.toString());
-			System.out.println("Error has been written out to the output file...");
-			RobotiumTranslator.toWrite.add("\t\t//Error: Information missing for tap action. " + e.toString());
+			RobotiumTranslator.toWrite.add("\t\tsolo.goBack();\n");
 		}
-		
-		return true;
-	}
-
-	@Override
-	public boolean longTap(StepTestCase testCase) throws Exception {
-
-		try 
+		else 
 		{
-			String id = testCase.getComponent().getId();
-			String type = testCase.getComponent().getType();
-			String index = testCase.getComponent().getIndex();
-			
-			throwEmptyInfo(Arrays.asList(id,type,index));
-			
-			RobotiumTranslator.toWrite.add("\t\t// " + id + "\n");
 			RobotiumTranslator.toWrite.add("\t\tcurView = solo.getView(" + type + ".class, " + index + ");\n");
-			RobotiumTranslator.toWrite.add("\t\tsolo.clickLongOnView(curView, 800);");
-			
-		} catch(Exception e) 
-		{
-			System.out.println("Information needed for tap action is missing. Error: " + e.toString());
-			System.out.println("Error has been written out to the output file...");
-			RobotiumTranslator.toWrite.add("\t\t//Error: Information missing for tap action. " + e.toString());
+			RobotiumTranslator.toWrite.add("\t\tsolo.clickOnView(curView);");
 		}
 	
 		return true;
 	}
+	
+	/**
+	 * Need: type, index
+	 */
+	@Override
+	public boolean longTap(StepTestCase testCase) throws Exception {
 
+		String id = testCase.getComponent().getId();
+		String type = testCase.getComponent().getType();
+		String index = testCase.getComponent().getIndex();
+		
+		writeTestName(id);
+		
+		RobotiumTranslator.toWrite.add("\t\t// " + id + "\n");
+		RobotiumTranslator.toWrite.add("\t\tcurView = solo.getView(" + type + ".class, " + index + ");\n");
+		RobotiumTranslator.toWrite.add("\t\tsolo.clickLongOnView(curView, 800);");
+
+		return true;
+	}
+
+	/**
+	 * Need: type, index, text
+	 */
 	@Override
 	public boolean pressKey(StepTestCase testCase) throws Exception {
 		
-		try
-		{
-			String id = testCase.getComponent().getId();
-			String type = testCase.getComponent().getType();
-			String index = testCase.getComponent().getIndex();
-			String textToType = testCase.getComponent().getText();
-
-			throwEmptyInfo(Arrays.asList(id,type,index,textToType));
-			RobotiumTranslator.toWrite.add("\t\t// " + id + "\n");
-			RobotiumTranslator.toWrite.add("\t\tcurView = solo.getView(" + type + ".class, " + index + ");\n");
-			RobotiumTranslator.toWrite.add("\t\tsolo.enterText(" + index + ", \"" + textToType + "\");");
-			
-		} catch(Exception e) 
-		{
-			System.out.println("Information needed for tap action is missing. Error: " + e.toString());
-			System.out.println("Error has been written out to the output file...");
-			RobotiumTranslator.toWrite.add("\t\t//Error: Information missing for tap action. " + e.toString());
-		}
+		String id = testCase.getComponent().getId();
+		String type = testCase.getComponent().getType();
+		String index = testCase.getComponent().getIndex();
+		String textToType = testCase.getComponent().getText();
+		
+		writeTestName(id);
+		
+		RobotiumTranslator.toWrite.add("\t\tcurView = solo.getView(" + type + ".class, " + index + ");\n");
+		RobotiumTranslator.toWrite.add("\t\tsolo.enterText(" + index + ", \"" + textToType + "\");");
 	
 		return true;
 	}
@@ -134,50 +96,15 @@ public class RobotiumFunctions extends GenericFunctions {
 		
 		return false;
 	}
-
-	@Override
-	public boolean flick(StepTestCase testCase) throws Exception {
 	
-		return false;
-	}
-
-	@Override
-	public boolean pinch(StepTestCase testCase) throws Exception {
-		return false;
-	}
-
-	@Override
-	public boolean scrollTo(StepTestCase testCase) throws Exception {
-		return false;
-	}
-
-	@Override
-	public boolean launchApp(StepTestCase testCase) throws Exception {
-		return false;
-	}
-
-	@Override
-	public boolean closeApp(StepTestCase testCase) throws Exception {
-		return false;
-	}
-
-	@Override
-	public boolean pressMenuKey(StepTestCase testCase) throws Exception {
-		return false;
-	}
-
-	@Override
-	public boolean hideKeyboard(StepTestCase testCase) throws Exception {
-		return false;
-	}
-	
-	private void throwEmptyInfo(List<String> a) throws Exception {
+	private void writeTestName(String id) {
 		
-		for(String s: a) {
-			if(s.equals("")) {
-				throw new Exception("invalid JSON FILE key-value pair...");
-			}
+		if(id != null && !id.equals("")) {
+			RobotiumTranslator.toWrite.add("\t\t// " + id + "\n");
+		}
+		else {
+			RobotiumTranslator.toWrite.add("\t\t// Unable to get test case name becase of missing component id\n");
 		}
 	}
-	
+
 }
