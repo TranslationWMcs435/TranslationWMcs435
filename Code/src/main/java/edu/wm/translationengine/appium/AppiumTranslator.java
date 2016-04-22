247179bc84476aeba964dd3b4136944dd101c1b9
VJ Davey <akdavey@email.wm.edu>
2016-04-21 21:55:03 -0400

    Update AppiumTranslator.java
    Now dynamically takes in user input for Appium system directory just like the Live mode and passes appName from the JSON file to allow more dynamic generation of testcase.
    setupTestMethodHeader is now bastardized and used to pass directory and appName. Will most likely be overriden in next sprint to additionally pass name of output file from main, so that a program named "foo.java" will start wth "public class foo{"
    Also I need to figure out how to escape user input so that directory does not need to have extra slashes added manually after test file generation

--- Code/src/main/java/edu/wm/translationengine/appium/AppiumTranslator.java
+++ Code/src/main/java/edu/wm/translationengine/appium/AppiumTranslator.java
@@ -1,6 +1,8 @@
 package edu.wm.translationengine.appium;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -33,11 +35,29 @@ public class AppiumTranslator extends GenericTranslator{
 		appName = testCase.getAppName();
 		packageName = testCase.getPackageName();
 		mainActivity = testCase.getMainActivity().substring(packageName.length() + 1);
+		String directory=null;
+		//ask user for appium directory on their system to make file more dynamic
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("DEFAULT APPIUM DIRECTORY : C:\\Program Files (x86)\\Appium\n"
+				+ "Use default directory? ");
+		String input = in.readLine();
+		if(input.matches("[y|Y].*")){
+			System.out.println("Using DEFAULT APPIUM DIRECTORY...");
+			directory = LiveModeServerWorker.getOSAppiumDir();
+		}else if(input.matches("[n|N].*")){
+			System.out.println("Okay. Enter the absolute path to the Appium directory on your system:");
+			directory = in.readLine();
+		}else{
+			System.out.println("Invalid Choice. Goodbye.");
+			System.exit(-1);
+		}
+		//-----------------------------------
 		List <StepTestCase> stepTestCases = testCase.getSteps();
 
 		//loop through the parsed input to generate the good stuff
+		//need to pass appName to setupTestMethodHeader. Dunno what to do with second arg.
 		fm.setupFileImports();
-		fm.setupTestMethodHeader(packageName, mainActivity);
+		fm.setupTestMethodHeader(directory, appName);
 		for(int i=0; i < stepTestCases.size(); i++){
 			StepTestCase cur = stepTestCases.get(i);
 			af.appium_switcher(cur.getAction(), cur);
